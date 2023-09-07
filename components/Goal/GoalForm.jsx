b6be31991961340fc74d06d3d6bd42fe636b773c
Muhammad I. Sayyed <muhammadisayyed@gmail.com>
2023-09-07 21:17:53 +0300

    Init prisma

--- components/Goal/GoalForm.jsx
+++ components/Goal/GoalForm.jsx
@@ -1,11 +1,12 @@
 function GoalForm() {
   return (
     <section>
-      <form action='submit' name='goal'>
+      <form>
         <label htmlFor='goal'>
           Write your goal here:
           <input type='text' id='goal' className='border border-black/25' />
         </label>
+        <button type='submit'>Add a goal</button>
       </form>
     </section>
   );
