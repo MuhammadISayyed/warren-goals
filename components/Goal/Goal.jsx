3d9d6afc53482ae222d5bd8481ed585e349c0aec
Muhammad I. Sayyed <muhammadisayyed@gmail.com>
2023-09-08 14:56:52 +0300

    Can prioritize & deprioritize goals

--- components/Goal/Goal.jsx
+++ components/Goal/Goal.jsx
@@ -1,5 +1,28 @@
-function Goal({ goal }) {
-  return <div>{goal.content}</div>;
-}
+'use client';
+
+import { dePrioritizeGoal, prioritizeGoal } from '@/utils/actions';
+import { useState } from 'react';
 
 export default Goal;
+
+function Goal({ goal }) {
+  const [isprioritized, setIsPrioritized] = useState(false);
+
+  function updateGoal() {
+    setIsPrioritized(!isprioritized);
+    if (isprioritized) {
+      dePrioritizeGoal(goal.id);
+    } else if (!isprioritized) {
+      prioritizeGoal(goal.id);
+    }
+  }
+
+  return (
+    <div className={`${goal.prioritized ? 'bg-green-300' : 'text-gray-400'}`}>
+      <p>{goal.content}</p>
+      <button onClick={() => updateGoal()}>
+        {goal.prioritized ? 'Deprioritize' : 'Prioritize'}
+      </button>
+    </div>
+  );
+}
