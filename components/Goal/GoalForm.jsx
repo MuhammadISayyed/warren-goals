54f5066c713cca5aa700a3fa6d3fc97edaee68aa
Muhammad I. Sayyed <muhammadisayyed@gmail.com>
2023-09-14 09:10:56 +0300

    Clear form input on submit

--- components/Goal/GoalForm.jsx
+++ components/Goal/GoalForm.jsx
@@ -1,12 +1,26 @@
+'use client';
+
 import { newGoal } from '@/utils/actions';
+import { useState } from 'react';
 
 function GoalForm() {
+  const [input, setInput] = useState('');
+  const handleSubmit = () => {
+    setInput('');
+  };
   return (
     <section>
-      <form action={newGoal}>
+      <form action={newGoal} onSubmit={handleSubmit}>
         <label htmlFor="goal">
           Write your goal here:
-          <input name="content" type="text" id="goal" className="border border-black/25" />
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            name="content"
+            type="text"
+            id="goal"
+            className="border border-black/25"
+          />
         </label>
         <button type="submit">Add a goal</button>
       </form>
